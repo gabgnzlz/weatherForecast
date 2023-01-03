b011b61ea366c8f5347591cc6f7e7d49886b4770
Gabriela Gonzalez <anagabrielagonzalezteran@gmail.com>
2023-01-03 21:21:17 +0100

    display of date, temperature, humidity

--- openweather/src/components/Card.js
+++ openweather/src/components/Card.js
@@ -12,6 +12,15 @@ const Card = ({loadingData, showData, weather, forecast}) => {
     var url = "";
     var iconUrl = "";
 
+    var iconUrl3 = "";
+    var iconUrl6 = "";
+    var iconUrl9 = "";
+
+    var forecastDate3 = "";
+    var forecastDate6 = "";
+    var forecastDate9 = "";
+
+
     if(loadingData){
         return <Spinner />;
     }
@@ -19,6 +28,13 @@ const Card = ({loadingData, showData, weather, forecast}) => {
     if(showData){
         url = "http://openweathermap.org/img/w/";
         iconUrl = url + weather.weather[0].icon + ".png"
+        iconUrl3 = url + forecast.list[1].weather[0].icon + ".png";
+        iconUrl6 = url + forecast.list[2].weather[0].icon + ".png";
+        iconUrl9 = url + forecast.list[3].weather[0].icon + ".png";
+
+        forecastDate3 = forecast.list[1].dt_txt.substring(8, 10) + '/' + forecast.list[1].dt_txt.substring(5, 7) + '/' + forecast.list[1].dt_txt.substring(0 , 4) + ' ' + forecast.list[1].dt_txt.substring(11 , 13);
+        forecastDate6 = forecast.list[2].dt_txt.substring(8, 10) + '/' + forecast.list[2].dt_txt.substring(5, 7) + '/' + forecast.list[2].dt_txt.substring(0 , 4) + ' ' + forecast.list[2].dt_txt.substring(11 , 13);
+        forecastDate9 = forecast.list[3].dt_txt.substring(8, 10) + '/' + forecast.list[3].dt_txt.substring(5, 7) + '/' + forecast.list[3].dt_txt.substring(0 , 4) + ' ' + forecast.list[3].dt_txt.substring(11 , 13);
     }
     return (
        <div className="mt-5">
@@ -38,7 +54,32 @@ const Card = ({loadingData, showData, weather, forecast}) => {
 
                                 <div className="col-md-8">
                                     <div className="card-body text-start mt-2">
+                                        <h5 className="card-text">Temperatura máxima: {(weather.main.temp_max - 273.15).toFixed(1)}</h5>
+                                        <h5 className="card-text">Temperatura mínima: {(weather.main.temp_min - 273.15).toFixed(1)}</h5>
+                                        <h5 className="card-text">Sensación térmica: {(weather.main.feels_like - 273.15).toFixed(1)}</h5>
+                                        <h5 className="card-text">Humedad: {weather.main.humidity}%</h5>
+                                        <h5 className="card-text">Velocidad del viento: {weather.wind.speed}m/s</h5>
                                     </div>
+                                    <hr/>
+
+                                <div className="row mt-4">
+                                    <div className="col">
+                                        <p>{forecastDate3}h</p>
+                                        <p className="description"><img src={iconUrl3} alt=""/> {forecast.list[1].weather[0].description}</p>
+                                        <p className="temp">{(forecast.list[1].main.temp - 273.15).toFixed(1)} ºC</p>
+                                    </div>
+                                    <div className="col">
+                                        <p>{forecastDate6}h</p>
+                                        <p className="description"><img src={iconUrl6} alt=""/> {forecast.list[2].weather[0].description}</p>
+                                        <p className="temp">{(forecast.list[2].main.temp - 273.15).toFixed(1)} ºC</p>
+                                    </div>
+                                    <div className="col">
+                                        <p>{forecastDate9}h</p>
+                                        <p className="description"><img src={iconUrl9} alt=""/> {forecast.list[3].weather[0].description}</p>
+                                        <p className="temp">{(forecast.list[3].main.temp - 273.15).toFixed(1)} ºC</p>
+                                    </div>
+                                </div>
+
                                 </div> 
                             </div>
                         </div>
